022c2c1e115716ea344bba0acdf6d32356fac9e1
Jonathan Reynolds <j14reynolds@gmail.com>
2015-10-24 23:54:29 -0500

    Error magically disappeared. Changed the signup query to utilize "or_" and created rudimentary tests for controllers.

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -3,6 +3,17 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 db.create_all()
+
+from controllers.user_controller import signup
+from controllers.user_controller import signin
+
+a = signup("Test","Tester", "tests4dayz", "tester@test.test", "testpw")
+if a is None:
+    print "signup success"
+b = signin("tests4dayz", "testpw")
+if b is "success":
+    print "signin success"
--- app/api/controllers/user_controller.py
+++ app/api/controllers/user_controller.py
@@ -1,16 +1,17 @@
 from api.models.user import User
 from api import db
+from sqlalchemy.sql.expression import or_
 
 def signup(firstname, lastname, username, email, password):
     newUser = User(firstname, lastname, username, email, password)
-    if User.query.filter_by(username = newUser.username).first() is None:
-        if User.query.filter_by(email = newUser.email).first() is None:
-            db.session.add(newUser)
-            db.session.commit()
-        else:
-            return "Error: email already exists."
-    else:
+    existingUser = User.query.filter(or_(User.username == newUser.username, User.email == newUser.email)).first()
+    if not existingUser:
+        db.session.add(newUser)
+        db.session.commit()
+    elif existingUser.username == username:
         return "Error: username already exists."
+    else:
+        return "Error: email already exists."
 
 def signin(name, password):
     user = User.query.filter_by(username = name).first()
