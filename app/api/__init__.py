f6266106d7192fede5d65acf270635fee9e67a1a
Jonathan Reynolds <j14reynolds@gmail.com>
2015-10-25 22:40:49 -0500

    Separate python programs in a more logical way.

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -8,15 +8,3 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
-db.create_all()
-
-#Test signup and signin
-from controllers.user_controller import signup
-from controllers.user_controller import signin
-
-a = signup("Test","Tester", "tests4dayz", "tester@test.test", "testpw")
-if a is None:
-    print "signup success"
-b = signin("tests4dayz", "testpw")
-if b is "success":
-    print "signin success"
