ddd10070453c1e3836cdf83b3f96a229a46a5e0c
Jonathan Reynolds <j14reynolds@gmail.com>
2015-11-11 01:04:25 -0600

    Created route for sign-up. Modified User model

--- app/api/models/user.py
+++ app/api/models/user.py
@@ -5,8 +5,8 @@ class User(Base):
     __tablename__ = 'auth_user'
     firstname= db.Column(db.String(80))
     lastname= db.Column(db.String(80))
-    username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
+    netid = db.Column(db.String(120), unique=True)
     # The PasswordType type implements passlib to securely hash passwords
     # Current scheme is sha512, but can be changed easily.
     password = db.Column(PasswordType(
@@ -14,11 +14,12 @@ class User(Base):
             'pbkdf2_sha512'
         ]))
 
-    def __init__(self, firstname, lastname, username, email, password):
+    def __init__(self, firstname, lastname, username, email, netid, password):
         self.firstname = firstname
         self.lastname = lastname
         self.username = username
         self.email = email
+        self.netid = netid
         self.password = password
 
     def __repr__(self):
