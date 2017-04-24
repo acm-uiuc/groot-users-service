5bdbca373c1f2c5a533ebd4bd2811a4c12d5e505
Sameet Sapra <sameetandpotatoes@gmail.com>
2017-04-24 18:01:39 -0500

    Rename yaml names

--- app.rb
+++ app.rb
@@ -39,7 +39,7 @@ set :bind, '0.0.0.0'
 configure :development do
     enable :unsecure
     
-    db = Config.load_config("development")    
+    db = Config.load_config("development_db")    
     DataMapper::Logger.new($stdout, :debug)
     DataMapper.setup(
         :default,
@@ -55,7 +55,7 @@ end
 configure :production do
     disable :unsecure
     
-    db = Config.load_config("production")
+    db = Config.load_config("production_db")
     DataMapper.setup(
         :default,
         "mysql://" + db["user"] + ":" + db["password"] + "@" + db["hostname"]+ "/" + db["name"]
